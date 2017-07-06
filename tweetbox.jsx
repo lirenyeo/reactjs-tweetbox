8e897784ff185b9c51095141885047bc02f2089d
Liren <liren90@gmail.com>
2017-07-06 15:32:42 +0800

    Implemented All Objectives

--- tweetbox.jsx
+++ tweetbox.jsx
@@ -1,12 +1,60 @@
 class TweetBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      text: "",
+      photoAdded: false
+    }  
+    this.handleTextInput = this.handleTextInput.bind(this)
+    this.togglePhoto = this.togglePhoto.bind(this)
+  }
+
+  handleTextInput(event) {
+    this.setState({text: event.target.value})
+  }
+
+  togglePhoto() {
+    this.setState({photoAdded: !this.state.photoAdded})
+  }
+
+  remainingChars() {
+    let count = this.state.photoAdded ? 140 - 23 : 140
+    return count - this.state.text.length
+  }
+
+  overflowAlert() {
+    let beforeOverflowText = this.state.text.substring(130, 140)
+    let afterOverflowText = this.state.text.substring(140)
+    if (this.state.photoAdded) {
+      beforeOverflowText = this.state.text.substring(130 - 23, 140 - 23)
+      afterOverflowText = this.state.text.substring(140 - 23)
+    }
+
+    if (this.remainingChars() < 0) {
+      return(
+        <div className="alert alert-warning" style={{wordBreak: 'break-all'}}>
+          You typed too much! ...&nbsp;
+          {beforeOverflowText}
+          <strong className="bg-danger">{afterOverflowText}</strong>
+        </div>
+      )
+    } else {
+      return ""
+    }
+  }
+
+
   render() {
+    let addPhotoButtonText = this.state.photoAdded? "✓ Photo Added" : "Add Photo"
+
     return (
       <div className="well clearfix">
-        <textarea className="form-control"></textarea>
+        { this.overflowAlert() }
+        <textarea className="form-control" onChange={ this.handleTextInput }></textarea>
         <br/>
-        <span>140</span>
-        <button className="btn btn-primary pull-right">Tweet</button>
-        <button className="btn btn-default pull-right">Add Photo</button>
+        <span>{ this.remainingChars() }</span>
+        <button className="btn btn-primary pull-right" disabled={ this.remainingChars() < 0 || this.remainingChars() == 140 }>Tweet</button>
+        <button className="btn btn-default pull-right" onClick={ this.togglePhoto } >{addPhotoButtonText}</button>
       </div>
     )
   }
